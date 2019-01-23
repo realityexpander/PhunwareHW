98d3fbeed2f735724b286c88e65d8f8c111129c5
Chris Athanas <realityexpander@gmail.com>
2019-01-23 02:46:20 -0600

    Add menu to main activity

--- app/src/main/java/com/realityexpander/phunwarehw/MainActivity.java
+++ app/src/main/java/com/realityexpander/phunwarehw/MainActivity.java
@@ -6,6 +6,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -95,4 +98,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+      return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      // Handle action bar item clicks here. The action bar will
+      // automatically handle clicks on the Home/Up button, so long
+      // as you specify a parent activity in AndroidManifest.xml.
+      int id = item.getItemId();
+
+      //noinspection SimplifiableIfStatement
+      if (id == R.id.action_settings) {
+        return true;
+      }
+
+      return super.onOptionsItemSelected(item);
+    }
+
 }
