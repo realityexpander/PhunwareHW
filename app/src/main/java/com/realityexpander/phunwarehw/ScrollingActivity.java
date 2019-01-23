65a97a231fade2d1df1268684b61a935b2e46a65
Chris Athanas <realityexpander@gmail.com>
2019-01-22 22:50:44 -0600

    Update ScrollingActivity.java

--- app/src/main/java/com/realityexpander/phunwarehw/ScrollingActivity.java
+++ app/src/main/java/com/realityexpander/phunwarehw/ScrollingActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -41,7 +42,12 @@ public class ScrollingActivity extends AppCompatActivity {
     TextView titleTextView = findViewById(R.id.title);
     TextView descriptionTextView = findViewById(R.id.description);
 
-    dateTextView.setText(thisStarEvent.getDate());
+    try {
+      dateTextView.setText( parse(thisStarEvent.getDate()).toString() );
+    } catch (ParseException e) {
+      e.printStackTrace();
+      dateTextView.setText(thisStarEvent.getDate());
+    }
     titleTextView.setText(thisStarEvent.getTitle());
     descriptionTextView.setText(thisStarEvent.getDescription());
 
@@ -76,6 +82,14 @@ public class ScrollingActivity extends AppCompatActivity {
 
   }
 
+  //      YYYY = four-digit year
+  //      MM   = two-digit month (01=January, etc.)
+  //      DD   = two-digit day of month (01 through 31)
+  //      hh   = two digits of hour (00 through 23) (am/pm NOT allowed)
+  //      mm   = two digits of minute (00 through 59)
+  //      ss   = two digits of second (00 through 59)
+  //      s    = one or more digits representing a decimal fraction of a second
+  //      TZD  = time zone designator (Z or +hh:mm or -hh:mm)
   public static Date parse(String input ) throws java.text.ParseException {
 
     //NOTE: SimpleDateFormat uses GMT[-+]hh:mm for the TZ which breaks
